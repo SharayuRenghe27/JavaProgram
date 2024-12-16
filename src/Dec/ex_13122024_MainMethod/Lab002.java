470d1b5cd1bcf4258fa21ed6e0af8627ebf996e4
hp <renghesharayu82@gmail.com>
2024-12-16 12:06:51 +0530

    16122024 commit

--- src/Dec/ex_13122024_MainMethod/Lab002.java
+++ src/Dec/ex_13122024_MainMethod/Lab002.java
@@ -2,6 +2,7 @@ package Dec.ex_13122024_MainMethod;
 
 public class Lab002 {
     public static void main(String[] args) {
-        System.out.println("2nd push");
+        System.out.println("2nd push on git");
+
     }
 }
