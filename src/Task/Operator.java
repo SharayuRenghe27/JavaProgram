be3970157c06895eaf92f535b2445432c0aa071a
hp <renghesharayu82@gmail.com>
2024-12-17 21:59:02 +0530

    17122024 commit

---
+++ src/Task/Operator.java
@@ -0,0 +1,21 @@
+package Task;
+
+public class Operator {
+
+    public static void main(String[] args) {
+        int a=101,b=10;
+        System.out.println("A=101 \t B=10 \n **Arithmetic Operator** \n Addition a+b="+a+b);
+        System.out.println("Substraction a-b="+a-b);
+        System.out.println("Multiplication a*b="+a*b);
+        System.out.println("Division a/b="+a/b);
+        System.out.println("Modulo a%b="+a%b);
+        System.out.println(" **Assignment Operator** \n Addition a+b="+a+b);
+        System.out.println("Modulo a%b="+a+=b);
+        System.out.println("Modulo a%b="+a-=b);
+
+
+
+
+
+    }
+}
