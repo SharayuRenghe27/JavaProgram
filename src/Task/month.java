22dd42e1949241e6c6c1916187f8b7b23c60eafc
hp <renghesharayu82@gmail.com>
2024-12-20 23:10:16 +0530

    20122024 commit

---
+++ src/Task/month.java
@@ -0,0 +1,58 @@
+package Task;
+
+import java.util.Scanner;
+
+public class month {
+    public static void main(String[] args) {
+        System.out.println("Enter no and I will tell you which month it is");
+
+        Scanner sc=new Scanner(System.in);
+        int monthno=sc.nextInt();
+        switch (monthno)
+        {
+            case 1:
+                System.out.println("Its January");
+                break;
+            case 2:
+                System.out.println("Its February ");
+                break;
+            case 3:
+                System.out.println("Its March");
+                break;
+            case 4:
+                System.out.println("Its April");
+                break;
+            case 5:
+                System.out.println("Its May");
+                break;
+            case 6:
+                System.out.println("Its June");
+                break;
+            case 7:
+                System.out.println("Its July");
+                break;
+            case 8:
+                System.out.println("Its August");
+                break;
+            case 9:
+                System.out.println("Its September");
+                break;
+            case 10:
+                System.out.println("Its October");
+                break;
+            case 11:
+                System.out.println("Its November");
+                break;
+            case 12:
+                System.out.println("Its December");
+                break;
+
+            default:
+                System.out.println("Add valid input");
+                break;
+
+
+
+        }
+    }
+}
