c9c283fbd9a71ec742b3f7cf3b46604a16a84b33
hp <renghesharayu82@gmail.com>
2024-12-21 20:29:30 +0530

    21122024 commit

---
+++ src/Dec/MainMethod/function1.java
@@ -0,0 +1,11 @@
+package Dec.MainMethod;
+
+public class function1
+{
+    public static void main(String[] args) {
+     greet2();
+    }
+     static void greet2(){
+        System.out.println("demo2");
+    }
+}
---
+++ src/Task/funtioncalculator.java
@@ -0,0 +1,33 @@
+package Task;
+
+import java.util.Scanner;
+
+public class funtioncalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter 2 values");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Add->"+add(a,b));
+        System.out.println("Sub->"+sub(a,b));
+        System.out.println("Mul->"+mul(a,b));
+        System.out.println("div->"+div(a,b));
+        sc.close();
+    }
+public static int add(int a, int b){
+    return a+b;
+}
+    public static int sub(int a, int b){
+        return a-b;
+    }
+    public static int mul(int a, int b){
+        return a*b;
+    }
+    public static int div(int a, int b){
+        if(a==0||b==0)
+            System.out.println("dont add 0");
+        return a/b;
+    }
+
+
+}
