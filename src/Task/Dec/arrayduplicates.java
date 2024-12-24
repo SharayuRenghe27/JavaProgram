3c1bd2a35002f2b309bacb4081b492895e8f10ab
hp <renghesharayu82@gmail.com>
2024-12-24 19:18:50 +0530

    24122024 commit

--- src/Task/Dec/arrayduplicates.java
+++ src/Task/Dec/arrayduplicates.java
@@ -2,17 +2,23 @@ package Task.Dec;
 
 import java.util.Arrays;
 
+
 public class arrayduplicates {
     public static void main(String[] args) {
-        int []array1={22,89,11,67,33,22,77};
+        int []array1={22,89,33,67,33,22,77};
         for (int p=0;p<array1.length;p++)
             System.out.print(" "+array1[p]);
         Arrays.sort(array1);
         System.out.println("\n Below are duplicates value---->");
-//        for (int i=1;i<array1.length;i++){
-//             if(args[i]==args[i]-1){
-//                System.out.println(args[i]);
-//            }
+
+        for (int i=0;i<array1.length;i++){
+           for (int j=i+1;j<(array1.length);j++){
+             if(array1[i]==array1[j]) {
+                 System.out.println (array1[j]+" ");
+
+             }
+             }
+            }
 
 
 
